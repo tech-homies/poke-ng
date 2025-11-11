c09c0749d49be54879ba235fa91bc69118d311f4
Benjamin Barbier <benjamin.barbier@partner.kiabi.com>
2025-11-11 22:16:59 +0100

    ✨ add nav template layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavComponent} from './layout/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
